6449b531b56639fcd3b1b445cf00299b29608004
Vivek <hyperwiz0p@gmail.com>
2026-06-08 13:20:43 +0530

    Day 61 Code Modified

--- Day61.py
+++ Day61.py
@@ -1,3 +1,84 @@
 #day 61 code
+"""
+Why Do Cloud Engineers Use It?
 
-print("day 61 code")
+Real examples:
+
+Check disk usage
+Check memory usage
+Restart services
+Take backups
+Execute shell scripts
+Gather system information
+
+Most automation tools use this concept."""
+
+
+import subprocess
+
+subprocess.run(["pwd"])
+"""
+subprocess.run(["ls"])"""
+
+"""Python
+   ↓
+subprocess.run()
+   ↓
+Linux Command
+   ↓
+Result"""
+
+#Capture Output
+"""
+Normally output goes to terminal.
+
+Let's store it in a variable."""
+
+import subprocess
+
+result = subprocess.run(["pwd"],capture_output=True,text=True)
+
+print(result.stdout)
+
+
+#Get Current User
+
+import subprocess
+
+result = subprocess.run(["whoami"],capture_output=True,text=True)
+
+print(result.stdout)
+
+
+#Check Disk Usage
+
+result = subprocess.run(["df","-h"],capture_output=True,text=True)
+
+print(result.stdout)
+
+#Check Memory Usage
+
+import subprocess
+"""
+result = subprocess.run(["free","-m"],capture_output=True,text=True)
+
+print(result.stdout)"""
+
+#Check return Code
+
+print(result.returncode)
+
+
+#Mini Project
+
+import subprocess
+try:
+    commands = [["pwd"],["whoami"],["date"]]
+
+    for command in commands:
+        result = subprocess.run(command,capture_output=True,text=True)
+
+    
+        print(result.stdout)
+except Exception as e:
+    print("An error orroured:",e)
--- Day61Assessment.py
+++ Day61Assessment.py
@@ -1,3 +1,27 @@
-#day 61 Assessment
+#System Information Collector
+"""
+Collect: pwd
+        whoami
+        uptime
+        df -h
 
-print("day 61 Assessment")
+
+Store results into: sample.txt"""
+
+import subprocess
+
+commands = [["pwd"],["whoami"],["uptime"],["df","-h"]]
+
+with open("sample.txt","a") as file:
+
+    for command in commands:
+
+        result = subprocess.run(command,capture_output=True,text=True)
+
+        print(result.stdout)
+
+        file.write(f"{command} : {result.stdout}\n")
+
+print("file created")
+
+
