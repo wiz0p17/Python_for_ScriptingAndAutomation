fbe31174a4d0878251f83488f03c160377bf3845
Vivek <hyperwiz0p@gmail.com>
2026-06-12 13:33:13 +0530

    Day 63 Code

--- Day63.py
+++ Day63.py
@@ -1,1 +1,91 @@
-print("Day 63 Code")
+#Day 63 – argparse (Build Professional Command-Line Tools)
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+
+args = parser.parse_args()
+
+print(args)"""
+
+
+#Add Your First Argument
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--name")
+
+args = parser.parse_args()
+
+print(args.name)"""
+
+
+"""
+Terminal
+    ↓
+--name Vivek
+    ↓
+argparse
+    ↓
+args.name
+    ↓
+Vivek"""
+
+
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--name",required=True,default="Vivek")
+
+parser.add_argument("--age",type=int)
+
+args = parser.parse_args()
+
+print(args.name)
+
+print(args.age)"""
+
+"""import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--server",required=True)
+
+args = parser.parse_args()
+
+print(f"Monitoring {args.server}")"""
+
+
+#boolean flags
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--cpu",action="store_true")
+
+args = parser.parse_args()
+
+print(args.cpu)"""
+
+
+#real Devops example
+
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--cpu",action="store_true")
+
+args = parser.parse_args()
+
+if args.cpu :
+    result = subprocess.run(["uptime"],capture_output=True,text=True)
+
+    print(f"CPU: {result.stdout}")
+
--- Day63Assessment.py
+++ Day63Assessment.py
@@ -1,1 +1,52 @@
-print("Day 63 Assessment")
+#Assignment (Cloud Engineer Version)
+"""
+Build:
+
+Health Checker
+
+Arguments:
+
+--cpu
+--memory
+--disk
+
+Example:
+
+python health.py --cpu
+
+Output:
+
+Checking CPU Usage
+python health.py --memory
+
+Output:
+
+Checking Memory Usage"""
+
+import argparse 
+import subprocess
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--cpu",action="store_true")
+
+parser.add_argument("--memory",action="store_true")
+
+parser.add_argument("--disk",action="store_true")
+
+args = parser.parse_args()
+
+if args.cpu:
+    result = subprocess.run(["uptime"],capture_output=True, text=True)
+
+    print(f"CPU: {result.stdout}")
+
+if args.memory:
+    result = subprocess.run(["free","-h"],capture_output=True, text=True)
+
+    print(f"Memory: {result.stdout}")
+
+if args.disk:
+    result = subprocess.run(["df","-h"],capture_output=True, text=True)
+
+    print(f"Disk: {result.stdout}")
