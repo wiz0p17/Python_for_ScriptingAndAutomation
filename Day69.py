4ff3771a5a2c3a5cf4e82c11f58ba7e4987c9631
Vivek <hyperwiz0p@gmail.com>
2026-06-21 22:06:13 +0530

    Day 69 Code

--- Day69.py
+++ Day69.py
@@ -1,3 +1,6 @@
 print("Day 69 Code")
 
 print("Today is my birthday!!!!")
+
+
+print("its the longest day on earth")
--- Day69Assessment.py
+++ Day69Assessment.py
@@ -2,3 +2,6 @@ print("Day 69 Assessment Code")
 
 
 print("Today is my birthday!!!!")
+
+
+print("its the longest day on earth")
