80b20afc988e695f3453ce4b603f7943c7f8645b
Vivek <hyperwiz0p@gmail.com>
2026-07-06 23:10:04 +0530

    Day 73 Code

--- Day73.py
+++ Day73.py
@@ -1,21 +1,33 @@
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
-print("Day 73 Code")
+"""import boto3
+
+ec2 = boto3.client("ec2")
+
+response = ec2.describe_instances()
+
+for reservations in response["Reservations"]:
+    for instance in reservations["Instances"]:
+        print(instance["InstanceId"])
+        print(instance["State"]["Name"])
+        print(instance.get("PublicIpAddress"))"""
 
 
+import boto3
 
+ec2 = boto3.client("ec2")
+
+response = ec2.describe_instances()
+
+for reservation in response["Reservations"]:
+
+    for instance in reservation["Instances"]:
+
+        print("-" * 40)
+
+        print("Instance ID:",
+              instance["InstanceId"])
+
+        print("State:",
+              instance["State"]["Name"])
+
+        print("Public IP:",
+              instance.get("PublicIpAddress"))
